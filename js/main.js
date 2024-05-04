25690f0e50be6298ae948bce56e6262a049b79c9
Pablo Caniffi <caniffipablo@gmail.com>
2024-05-04 15:51:00 -0300

    Correcciones menores

--- js/main.js
+++ js/main.js
@@ -5,21 +5,21 @@ let verstappen = 7;
 function campeonato (autoGanador) {
     let mi_coche = 0;
     do {
-        let pregunta1 = parseInt(prompt("Elige el número del auto: 1-Williams 2-HAAS 3-McLaren 4-Mercedes 5-Ferrari" ));
+        let pregunta1 = parseInt(prompt("Elige el número del auto: \n1-Williams \n2-HAAS \n3-McLaren \n4-Mercedes \n5-Ferrari" ));
             if (pregunta1 < 3 || pregunta1 > 5){
                 alert ("Con ese auto no vas a ganar.");
                 mi_coche=0;
             } else {
                 mi_coche=pregunta1;
             }
-            let pregunta2 = parseInt(prompt("Elige el número de tu piloto: 1-Zhou 2-Sargeant 3-Perez 4-Hamilton 5-Alonso"));
+            let pregunta2 = parseInt(prompt("Elige el número de tu piloto: \n1-Zhou \n2-Sargeant \n3-Perez \n4-Hamilton \n5-Alonso"));
             if (pregunta2 < 3 || pregunta2 > 5 ){
                 alert ("Con ese piloto no vas a ganar.");
                 mi_coche=0;
             } else {
                 mi_coche= mi_coche + pregunta2;
             }
-            let pregunta3 = parseInt(prompt("Elige el circuito: 1-Imola 2-Suzuka 3-Spa 4-Silverston 5-Monaco "));
+            let pregunta3 = parseInt(prompt("Elige el circuito: \n1-Imola \n2-Suzuka \n3-Spa \n4-Silverston \n5-Monaco "));
             if (pregunta3 < 1 || pregunta3 > 5 ){
                 alert ("Circuito no válido.");
                 mi_coche=0;
