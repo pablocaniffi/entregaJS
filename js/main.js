972fd032722b0421e430be4b64f66b0c77ad45b0
Pablo Caniffi <caniffipablo@gmail.com>
2024-05-20 17:00:43 -0300

    Proyecto 2entrega

--- js/main.js
+++ js/main.js
@@ -1,44 +1,74 @@
-alert ("Podés ganarle a Verstappen?");
+alert ("Simulador de carrera:");
 
-let verstappen = 7;
+function competidor (piloto , escuderia , puntaje) {
+    this.piloto = piloto,
+    this.escuderia = escuderia,
+    this.puntaje = puntaje
+};
+
+let autoGanador = [];
+autoGanador.push (new competidor("Verstappen" , "Red Bull" , 9));
+autoGanador.push (new competidor("Russell" , "Mercedes" , 7));
+autoGanador.push (new competidor("Ocon" , "Alpine" , 4));
+
 
 function campeonato (autoGanador) {
-    let mi_coche = 0;
-    do {
-        let pregunta1 = parseInt(prompt("Elige el número del auto: \n1-Williams \n2-HAAS \n3-McLaren \n4-Mercedes \n5-Ferrari" ));
-            if (pregunta1 < 3 || pregunta1 > 5){
-                alert ("Con ese auto no vas a ganar.");
-                mi_coche=0;
-            } else {
-                mi_coche=pregunta1;
-            }
-            let pregunta2 = parseInt(prompt("Elige el número de tu piloto: \n1-Zhou \n2-Sargeant \n3-Perez \n4-Hamilton \n5-Alonso"));
-            if (pregunta2 < 3 || pregunta2 > 5 ){
-                alert ("Con ese piloto no vas a ganar.");
-                mi_coche=0;
-            } else {
-                mi_coche= mi_coche + pregunta2;
-            }
-            let pregunta3 = parseInt(prompt("Elige el circuito: \n1-Imola \n2-Suzuka \n3-Spa \n4-Silverston \n5-Monaco "));
-            if (pregunta3 < 1 || pregunta3 > 5 ){
-                alert ("Circuito no válido.");
-                mi_coche=0;
-            } else {
-                mi_coche= mi_coche/pregunta3;
-            }
-
     
-            if (mi_coche > autoGanador){
-                console.log ("GANASTE: sos ingeniero de pista.");
-            }else if (mi_coche > 3) {
-                console.log ("PERDISTE: Estuviste cerca, mala detención en boxes.");
-            }else{
-                console.log ("PERDISTE: No sos parte de una escudería competitiva...");
-            }
+    do {
+        let rival = parseInt(Math.round(prompt("Elige el número de tu rival: \n1-"+autoGanador[0].piloto+" con "+autoGanador[0].escuderia + "\n2-"+autoGanador[1].piloto+" con "+autoGanador[1].escuderia + "\n3-"+autoGanador[2].piloto+" con "+autoGanador[2].escuderia)));
+    
+        if (rival>0 && rival<=autoGanador.length){
+        
+        let mi_coche = 0;
+        let mi_escuderia = ["Williams" , "HAAS" , "McLaren" , "Mercedes" , "Ferrari"];
+        let mi_piloto = ["Zhou" , "Sargeant" , "Pérez" , "Hamilton" , "Alonso"];
+        
+            let pregunta1 = parseInt(Math.round(prompt("Elige el número de tu auto: \n1-"+ mi_escuderia[0]+ "\n2-"+mi_escuderia[1]+ "\n3-"+mi_escuderia[2] +"\n4-"+mi_escuderia[3]+ "\n5-"+mi_escuderia[4] )));
+                if (pregunta1 < 3 || pregunta1 > 5){
+                    alert ("Con ese auto no vas a ganar.");
+                    mi_coche=0;
+                } else {
+                    mi_coche=pregunta1;
+                }
+                let pregunta2 = parseInt(Math.round(prompt("Elige el número de tu piloto: \n1-"+mi_piloto[0]+"\n2-"+mi_piloto[1] +"\n3-"+mi_piloto[2]+"\n4-"+mi_piloto[3]+"\n5-"+mi_piloto[4])));
+                if (pregunta2 < 3 || pregunta2 > 5 ){
+                    alert ("Con ese piloto no vas a ganar.");
+                    mi_coche=0;
+                } else {
+                    mi_coche= mi_coche + pregunta2;
+                }
+                let pregunta3 = parseInt(prompt("Elige el circuito: \n1-Imola \n2-Suzuka \n3-Spa \n4-Silverston \n5-Monaco "));
+                if (pregunta3 < 1 || pregunta3 > 5 ){
+                    alert ("Circuito no válido.");
+                    mi_coche=0;
+                } else {
+                    mi_coche= mi_coche/pregunta3;
+                }
+
+                let declaracion1 = mi_piloto.filter((variable) =>{return variable === mi_piloto[pregunta2-1]} );
+                let declaracion2 = mi_escuderia.filter((variable)=>{return variable === mi_escuderia[pregunta1-1]});
+                let declaracion = declaracion1.concat(declaracion2);
+    
+                alert("Se generó una carrera entre " + declaracion + " vs " + autoGanador[rival-1].piloto + "," + autoGanador[rival-1].escuderia);
+    
+        
+                if (mi_coche > autoGanador[rival-1].puntaje){
+                    console.log ("GANASTE: sos ingeniero de pista.");
+                }else if (mi_coche > 3) {
+                    console.log ("PERDISTE: Estuviste cerca, mala detención en boxes.");
+                }else{
+                    console.log ("PERDISTE: No sos parte de una escudería competitiva...");
+                }
+                
+                control = prompt("Seguimos jugando? S/N").toUpperCase();            
+        } else{
+            alert ("No ingresó un número válido. \nIntente nuevamente.");
+        };
+    
+    }
+    while (control === "S");
             
-            control = prompt("Seguimos jugando? S/N").toUpperCase();            
-        }        
-        while (control === "S");
 };   
 
-campeonato(verstappen);
+
+campeonato(autoGanador);
