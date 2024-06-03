0f440fedb3ffd2e69d1e6ae0d0df020236997f83
Pablo Caniffi <caniffipablo@gmail.com>
2024-06-03 18:40:02 -0300

    Header Nav Main

--- js/index.js
+++ js/index.js
@@ -21,12 +21,22 @@ imagenHeader.classList.add("col-3");
 
 let h1Titulo = document.createElement("h1");
 h1Titulo.classList.add("tituloPrincipal");
-h1Titulo.classList.add("col-9");
+h1Titulo.classList.add("col-7");
 h1Titulo.innerText = "Formula 1";
 
 divRowHeader.appendChild(imagenHeader);
 divRowHeader.appendChild(h1Titulo);
 // fin header
+
+
+// nav
+let nav = document.querySelector(".barraNav");
+let itemsNombre = nav.querySelectorAll("h2");
+let itemsRedes = nav.querySelectorAll("a");
+itemsNombre.forEach((item)=>item.classList.add("redes"));
+itemsRedes.forEach((item)=>item.classList.add("redes"));
+miBody.appendChild(nav);
+// fin nav
 
 
 
@@ -36,15 +46,6 @@ lateral.classList.add("lateral");
 miBody.appendChild(lateral);
 // fin aside
 
-
-
-// nav
-let nav = document.querySelector(".encabezado");
-let itemsNombre = nav.querySelectorAll("h2");
-let itemsRedes = nav.querySelectorAll("a");
-itemsNombre.forEach((item)=>item.classList.add("redes"));
-itemsRedes.forEach((item)=>item.classList.add("redes"));
-// fin nav
 
 
 // main
@@ -57,41 +58,47 @@ divRowMain.classList.add("row");
 
 
 // Título "Pilotos"
+let contenedorH3 = document.createElement("div");
+contenedorH3.classList.add("ubiH3");
+contenedorH3.classList.add("col-12");
 let h3Titulo = document.createElement("h3");
 h3Titulo.classList.add("tituloPrincipal");
-h3Titulo.classList.add("col-12");
+h3Titulo.classList.add("col-5");
 h3Titulo.innerText = "Pilotos".toUpperCase();
-divRowMain.appendChild(h3Titulo);
+contenedorH3.appendChild(h3Titulo);
+divRowMain.appendChild(contenedorH3);
 principal.appendChild(divRowMain);
 
 // Cards Pilotos
-function piloto (nombre , escuderia , numero) {
+function piloto (nombre , escuderia , numero , url) {
     this.nombre = nombre,
     this.escuderia = escuderia,
-    this.numero = numero
+    this.numero = numero,
+    this.url = url
 };
 
 let pilotos = [];
-pilotos.push(new piloto("Esteban Ocon" , "Alpine" , "Número 31"));
-pilotos.push(new piloto("Pierre Gasly" , "Alpine" , "Número 10"));
-pilotos.push(new piloto("Fernando Alonso" , "Aston Martin" , "Número 14"));
-pilotos.push(new piloto("Lance Stroll" , "Aston Martin" , "Número 18"));
-pilotos.push(new piloto("Charles Lecler" , "Ferrari" , "Número 16"));
-pilotos.push(new piloto("Carlos Sainz" , "Ferrari" , "Número 55"));
-pilotos.push(new piloto("Kevin Magnussen" , "HAAS F1 Team" , "Número 20"));
-pilotos.push(new piloto("Nico Hulkenberg" , "HAAS F1 Team" , "Número 27"));
-pilotos.push(new piloto("Lando Norris" , "McLaren F1" , "Número 4"));
-pilotos.push(new piloto("Oscar Piastri" , "McLaren F1" , "Número 81"));
-pilotos.push(new piloto("Lewis Hamilton" , "Mercedes" , "Número 44"));
-pilotos.push(new piloto("George Russell" , "Mercedes" , "Número 63"));
-pilotos.push(new piloto("Max Verstappen" , "Red Bull Racing" , "Número 1"));
-pilotos.push(new piloto("Sergio Pérez" , "Red Bull Racing" , "Número 11"));
-pilotos.push(new piloto("Guanyu Zhou" , "Stake F1 Team" , "Número 24"));
-pilotos.push(new piloto("Valteri Bottas" , "Stake F1 Team" , "Número 77"));
-pilotos.push(new piloto("Daniel Ricciardo" , "Scuderia Alpha Tauri" , "Número 3"));
-pilotos.push(new piloto("Yuki Tsunoda" , "Scuderia Alpha Tauri" , "Número 22"));
-pilotos.push(new piloto("Logan Sargeant" , "Willliams" , "Número 2"));
-pilotos.push(new piloto("Alexander Albon" , "Williams" , "Número 23"));
+pilotos.push(new piloto("Esteban Ocon" , "Alpine" , "Número 31" , '<img class="fotoPiloto" src="./multimedia/1esteban-ocon.webp" alt="Sargeant">'));
+pilotos.push(new piloto("Pierre Gasly" , "Alpine" , "Número 10", '<img class="fotoPiloto" src="./multimedia/1pierre-gasly.webp" alt="Sargeant">'));
+pilotos.push(new piloto("Fernando Alonso" , "Aston Martin" , "Número 14", '<img class="fotoPiloto" src="./multimedia/2fernando-alonso.webp" alt="Sargeant">'));
+pilotos.push(new piloto("Lance Stroll" , "Aston Martin" , "Número 18", '<img class="fotoPiloto" src="./multimedia/2lance-stroll.webp" alt="Sargeant">'));
+pilotos.push(new piloto("Charles Leclerc" , "Ferrari" , "Número 16", '<img class="fotoPiloto" src="./multimedia/3charles-leclerc.webp" alt="Sargeant">'));
+pilotos.push(new piloto("Carlos Sainz" , "Ferrari" , "Número 55", '<img class="fotoPiloto" src="./multimedia/3carlos-sainz.webp" alt="Sargeant">'));
+pilotos.push(new piloto("Kevin Magnussen" , "HAAS F1 Team" , "Número 20", '<img class="fotoPiloto" src="./multimedia/4kevin-magnussen.webp" alt="Sargeant">'));
+pilotos.push(new piloto("Nico Hulkenberg" , "HAAS F1 Team" , "Número 27", '<img class="fotoPiloto" src="./multimedia/4nico-hulkenberg.webp" alt="Sargeant">'));
+pilotos.push(new piloto("Lando Norris" , "McLaren F1" , "Número 4", '<img class="fotoPiloto" src="./multimedia/5lando-norris.webp" alt="Sargeant">'));
+pilotos.push(new piloto("Oscar Piastri" , "McLaren F1" , "Número 81", '<img class="fotoPiloto" src="./multimedia/5oscar-piastri.webp" alt="Sargeant">'));
+pilotos.push(new piloto("Lewis Hamilton" , "Mercedes" , "Número 44", '<img class="fotoPiloto" src="./multimedia/6lewis-hamilton.webp" alt="Sargeant">'));
+pilotos.push(new piloto("George Russell" , "Mercedes" , "Número 63", '<img class="fotoPiloto" src="./multimedia/6george-russell.webp" alt="Sargeant">'));
+pilotos.push(new piloto("Max Verstappen" , "Red Bull Racing" , "Número 1", '<img class="fotoPiloto" src="./multimedia/7max-verstappen.webp" alt="Sargeant">'));
+pilotos.push(new piloto("Sergio Pérez" , "Red Bull Racing" , "Número 11", '<img class="fotoPiloto" src="./multimedia/7sergio-perez.webp" alt="Sargeant">'));
+pilotos.push(new piloto("Guanyu Zhou" , "Stake F1 Team" , "Número 24", '<img class="fotoPiloto" src="./multimedia/8guanyu-zhou.webp" alt="Sargeant">'));
+pilotos.push(new piloto("Valteri Bottas" , "Stake F1 Team" , "Número 77", '<img class="fotoPiloto" src="./multimedia/8valtteri-bottas.webp" alt="Sargeant">'));
+pilotos.push(new piloto("Daniel Ricciardo" , "Scuderia Alpha Tauri" , "Número 3", '<img class="fotoPiloto" src="./multimedia/9ricciardo.webp" alt="Sargeant">'));
+pilotos.push(new piloto("Yuki Tsunoda" , "Scuderia Alpha Tauri" , "Número 22", '<img class="fotoPiloto" src="./multimedia/9yuki-tsunoda.webp" alt="Sargeant">'));
+pilotos.push(new piloto("Logan Sargeant" , "Willliams" , "Número 2", '<img class="fotoPiloto" src="./multimedia/10logan-sargeant.webp" alt="Sargeant">'));
+pilotos.push(new piloto("Alexander Albon" , "Williams" , "Número 23", '<img class="fotoPiloto" src="./multimedia/10alexander-albon.webp" alt="Sargeant">'));
+
 
 function competidor (conductor) {
     let i = 0;
@@ -104,9 +111,12 @@ function competidor (conductor) {
 
         let foto = document.createElement("div");
         foto.classList.add("foto");
+        foto.classList.add("text-center");
+        foto.innerHTML = conductor[i].url;
     
-        let info = document.createElement("ul");
-        info.classList.add("info")
+        let info = document.createElement("div");
+        info.classList.add("info");
+        info.classList.add("text-center");
         let infoNombre = document.createElement("li");
         infoNombre.innerText = conductor[i].nombre;
         let infoEscuderia = document.createElement("li");
